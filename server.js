4c19bc8241677d1169f99f1f7b213720518a0f0d
Eric Nunes <eric26052004@gmail.com>
2023-07-24 02:07:49 -0300

    1.0.1

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import { Server } from "socket.io";
 const app = express();
 const server = http.createServer(app);
 const sockets = new Server(server);
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.use(express.static("public"));
 
